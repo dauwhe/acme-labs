a4a19b45e16646b19010676c687c132ee6a0eaf2
BigBlueHat <byoung@bigbluehat.com>
2016-11-08 11:32:16 -0500

    Code styled page.js

--- page.js
+++ page.js
@@ -3,31 +3,29 @@
   var thisScriptURL = document.currentScript.src;
   // some nasty url hacking that should probably be done some other way
   var urlRE = /^.*acme\/([^\/]+)\//.exec(location.href);
-  var publicationBaseURL = location.origin + '/epub-zero/acme/'
- // var publicationBaseURL = '/' + getPathByName() + '/';
-  
-  //what folder is the manifest in
-function getPathByName() {
+  var publicationBaseURL = location.origin + '/epub-zero/acme/';
+  // var publicationBaseURL = '/' + getPathByName() + '/';
 
-//this is deeply flawed. fails on MobyDick/folder/manifest.json
- var path = RegExp('[?]manifest=' + '([^/]*)')
-                    .exec(window.location.search);
-    return path ?
-        decodeURIComponent(path[1].replace(/\+/g, ' '))
-        : null;
-}
+  //what folder is the manifest in
+  function getPathByName() {
+    // this is deeply flawed. fails on MobyDick/folder/manifest.json
+    var path = RegExp('[?]manifest=' + '([^/]*)')
+                 .exec(window.location.search);
+    return path
+      ? decodeURIComponent(path[1].replace(/\+/g, ' '))
+      : null;
+  }
 
   var publicationName = getPathByName();
   var ui = document.getElementById('page-controls');
   var uiEpub = document.getElementById('epub-controls');
-  
- // navigator.serviceWorker.register(new URL('sw.js', thisScriptURL));
- navigator.serviceWorker.register('kroner.js');
+
+  // navigator.serviceWorker.register(new URL('sw.js', thisScriptURL));
+  navigator.serviceWorker.register('kroner.js');
 
   if (navigator.serviceWorker.controller) {
     initPageControls();
-  }
-  else {
+  } else {
     navigator.serviceWorker.addEventListener('controllerchange', function onControllerChange() {
       navigator.serviceWorker.removeEventListener('controllerchange', onControllerChange);
       initPageControls();
@@ -37,11 +35,11 @@ function getPathByName() {
   function initPageControls() {
     caches.has(publicationName).then(function(isCached) {
       ui.innerHTML =
-        '<span><label class="status"><input type="checkbox" class="work-offline">Save</label></span>' +
-              '<form method="get" action="' + publicationBaseURL + publicationName + '/download-publication"><button type="submit">Download</button></form>'
+        '<span><label class="status"><input type="checkbox" class="work-offline">Save</label></span>'
+        + '<form method="get" action="' + publicationBaseURL + publicationName
+        + '/download-publication"><button type="submit">Download</button></form>';
+        //      '<span class="download"><a href="' + publicationBaseURL + publicationName + '/download-publication">Download</a></span>';
 
-  //      '<span class="download"><a href="' + publicationBaseURL + publicationName + '/download-publication">Download</a></span>';
-      
       var status = ui.querySelector('.status');
       var checkbox = ui.querySelector('.work-offline');
       checkbox.checked = isCached;
@@ -50,36 +48,37 @@ function getPathByName() {
         if (!this.checked) {
           caches.delete(publicationName);
           status.textContent = "Removed";
-        }
-        else {
+        } else {
          status.textContent = "Offlinifying";
-          fetch(publicationBaseURL + publicationName + '/manifest.json', { mode: 'no-cors' }).then(function(response) {
-            return response.json();
-          }).then(function(dave) {
-          
-    newArray = dave.resources.map(function(el) { return el.href});
-    
-return dave.spine.map(function(el) { return el.href}).concat(newArray);
+          fetch(publicationBaseURL + publicationName + '/manifest.json', { mode: 'no-cors' })
+            .then(function(response) {
+              return response.json();
+            })
+            .then(function(dave) {
+              newArray = dave.resources.map(function(el) { return el.href});
+              return dave.spine.map(function(el) { return el.href}).concat(newArray);
+            })
+            .then(function(data) {
+              data.push('manifest.json');
+              publicationURL = publicationBaseURL + publicationName;
 
-}).then(function(data) {
-            data.push('manifest.json');           
-            publicationURL = publicationBaseURL + publicationName;
-
-            return caches.open(publicationName).then(function(cache) {
-              return cache.addAll(data.map(function(url) {              
-                return new URL(publicationName + '/' + url, publicationURL);
-              }));
+              return caches.open(publicationName).then(function(cache) {
+                return cache.addAll(data.map(function(url) {
+                  return new URL(publicationName + '/' + url, publicationURL);
+                }));
+              });
+            })
+            .then(function() {
+              // status.textContent = 'Offlinification complete!';
+              alert("Offlinification complete!");
+              status.textContent = "Saved";
+            })
+            .catch(function(err) {
+              console.log(err);
+              // status.textContent = 'Offlinification failed :(';
+              alert("Offlinification failed!");
+              status.textContent = "";
             });
-          }).then(function() {
-            // status.textContent = 'Offlinification complete!';
-            alert("Offlinification complete!");
-            status.textContent = "Saved";
-          }).catch(function(err) {
-            console.log(err);
-            // status.textContent = 'Offlinification failed :(';
-            alert("Offlinification failed!");
-            status.textContent = "";
-          });
         }
       });
     });
